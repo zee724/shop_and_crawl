b5b34a93d97f9b30113fe23618b34f564bbc344d
Steven-Ubuntu <46341377@qq.com>
2017-01-20 17:00:14 +0800

    Add post function

--- tomcrawl.js
+++ tomcrawl.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var myconfig = require('./config.js')
-
+var bodyParser = require('body-parser');
+var markdown = require("markdown").markdown;
 var app = express();
+
+
+// see https://github.com/expressjs/body-parser
+// 添加 body-parser 中间件就可以了
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 //set up mysql
@@ -51,6 +58,95 @@ app.get('/about', function(req,res){
 	
 
 });
+
+app.get('/post/', function(req,res){
+
+	//创建连接  
+	var client = mysql.createConnection({  
+	  user: myconfig.myconfig.db_user,  
+	  password: myconfig.myconfig.db_passwd,  
+	});
+
+	client.connect();
+	client.query("use " + TEST_DATABASE);
+	
+	client.query(  
+	  'SELECT * FROM sm_post ORDER BY created desc',  
+	  function selectCb(err, results, fields) {  
+	    if (err) {  
+	      throw err;  
+	    }  
+	      
+	      if(results)
+	      {
+		  res.render('post',{results:results});
+	      }    
+	    client.end(); 
+	});
+	
+
+});
+
+app.get('/post/:postId', function(req,res){
+	var postId = req.params.postId;
+	
+	//创建连接  
+	var client = mysql.createConnection({  
+	  user: myconfig.myconfig.db_user,  
+	  password: myconfig.myconfig.db_passwd,  
+	});
+
+	client.connect();
+	client.query("use " + TEST_DATABASE);
+	
+	client.query(  
+	  'SELECT * FROM sm_post WHERE id = ' + postId ,  
+	  function selectCb(err, results, fields) {  
+	    if (err) {  
+	      throw err;  
+	    }  
+	      
+	      if(results)
+	      {
+		rs = results[0];
+		title = rs.title;
+		content = markdown.toHTML(rs.content);
+		created = rs.created;
+		res.render('postid',{title:title,content:content,created:created});
+	      }    
+	    client.end(); 
+	});
+
+});
+
+app.get('/editpost', function(req,res){
+	res.render('editpost');
+});
+
+app.post('/editpost', function(req,res){
+    	res.render('editpost'); 
+
+
+	//创建连接  
+	var client = mysql.createConnection({  
+	  user: myconfig.myconfig.db_user,  
+	  password: myconfig.myconfig.db_passwd,  
+	});
+
+	client.connect();
+	client.query("use " + TEST_DATABASE);
+	
+	var sql = 'INSERT INTO sm_post SET title=?,content=?',values = [req.body.title, req.body.content];
+	client.query(  
+	  sql,values,  
+	  function selectCb(err, results, fields) {  
+	    if (err) {  
+	      throw err;  
+	    }
+	    client.end(); 
+	});
+});
+
 
 
 // 404 catch-all handler (middleware)
